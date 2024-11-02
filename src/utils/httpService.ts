3154880d1aa95d6807dcc2b6b9ba92ad8a2c43aa
Hemit Patel <patelvhemit@gmail.com>
2024-11-02 17:20:57 -0400

    Update httpService.ts

--- src/utils/httpService.ts
+++ src/utils/httpService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const httpService = axios.create({
-    baseURL: process.env.API_URL, // Backend URL
+    baseURL: process.env.API_URL as string, // Backend URL
     withCredentials: true, // Enable sending cookies for cross-origin requests
 });
 
