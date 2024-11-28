7ca3fe115f9e0d63a6a75d8cb035856867e59460
Hemit Patel <patelvhemit@gmail.com>
2024-11-28 18:51:24 +0000

    changed how navitems are added through JSON list

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,5 +1,17 @@
 "use client";
-import Image from 'next/image'
+import Image from 'next/image';
+
+// Define the menu items in JSON format
+const menuItems = [
+  {
+    label: "Home",
+    href: "#"
+  },
+  {
+    label: "About",
+    href: "#"
+  },
+];
 
 const NavBar = () => {
   return (
@@ -30,26 +42,16 @@ const NavBar = () => {
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
-                Contact
-              </a>
-            </li>
+            {menuItems.map((item, index) => (
+              <li key={index}>
+                <a
+                  href={item.href}
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
