1c888040362fb3240965f52c84383c6a7ea417fe
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-11-29 13:01:29 -0500

    fully designed about pag

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,63 @@
+'use client'
+
 import Image from 'next/image';
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const About = () => {
+  // Array of team members
+  const team = [
+    {
+      name: 'Aarush Kute',
+      role: 'CEO',
+      imgSrc: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png',
+      linkedIn: "https://www.linkedin.com/in/aarush-kute-1639a525b/"
+    },
+    {
+      name: 'Hemit Patel',
+      role: 'COO',
+      imgSrc: 'https://media.licdn.com/dms/image/v2/D5603AQFDJxp7vmgX0Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1695640460890?e=1738195200&v=beta&t=sfsEBemwKB0_r70CJcFTqoBPOa2LgWU46Tq-CtjgJek',
+      linkedIn: "https://www.linkedin.com/in/hemit-patel-383ab3271/"
+    }, 
+    {
+      name: "Lakshaya Jain",
+      role: "Intern",
+      imgSrc: "https://media.licdn.com/dms/image/v2/D4E35AQE94v14CxnoKA/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1712092985933?e=1733508000&v=beta&t=5a4g6lkHC-vQfmIaZkp5fPlIR8uA4jcxo7UKfRV-3NE",
+      linkedIn: "https://www.linkedin.com/in/lakshya-jain-9a66a22a5/"
+    },
+    
+  ];
+
+  const router = useRouter()
+
+  const handleGoToHome = () => {
+    router.push("/")
+  }
+
   return (
     <div>
+        <section className="py-24 relative">
+        <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+            <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
+                <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
+                    <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
+                        <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">ACE the SAT.</h2>
+                        <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">Our product will help you prep for the SAT through providing a question bank! You will have annotation and other tools available to aid you in your SAT journey to your dream school 🚀</p>
+                    </div>
+                    <button className="sm:w-fit w-full px-3.5 py-2 bg-blue-600 hover:bg-blue-800 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex" onClick={handleGoToHome}>
+                        <span className="px-1.5 text-white text-sm font-medium leading-6">Get Started</span>
+                    </button>
+                </div>
+                <Image 
+                    className='lg:mx-0 mx-auto h-full rounded-3xl object-cover" src="https://pagedone.io/asset/uploads/1717751272.png'
+                    src="https://images.unsplash.com/photo-1606326608606-aa0b62935f2b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="About Us image"
+                    width={500}
+                    height={300}
+                />
+            </div>
+        </div>
+    </section>
       <section className="py-24 relative">
         <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
           <div className="w-full justify-start items-center gap-12 grid lg:grid-cols-2 grid-cols-1">
@@ -12,9 +66,9 @@ const About = () => {
                 <Image
                   className="rounded-xl object-cover"
                   src="https://images.unsplash.com/photo-1604933834215-2a64950311bd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt="About Us image"
                   width={500}
                   height={300}
+                  alt='Image of person'
                 />
               </div>
               <Image
@@ -50,6 +104,41 @@ const About = () => {
           </div>
         </div>
       </section>
+
+      <section className="bg-white">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+            <div className="mx-auto mb-8 max-w-screen-sm lg:mb-16">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Our team</h2>
+            <p className="font-light text-gray-500 sm:text-xl">
+                Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind
+            </p>
+            </div>
+            {/* Mapping over team array */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            {team.map((member, index) => (
+                <div key={index} className="flex flex-col items-center justify-center">
+                <img
+                    className="w-36 h-36 rounded-full mb-4"
+                    src={member.imgSrc}
+                    alt={member.name}
+                />
+                <div>
+                    <h3 className="text-2xl font-bold tracking-tight text-gray-900">{member.name}</h3>
+                    <p>{member.role}</p>
+                    <ul className="flex justify-center mt-4 space-x-4">
+                    <li>
+                        <a href={member.linkedIn} className="text-[#0e76a8] hover:text-gray-900">
+                        {/* LinkedIn icon */}
+                        <svg fill="#0090b1" height="15px" width="15px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 310 310" xmlSpace="preserve" stroke="#0090b1"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="XMLID_801_"> <path id="XMLID_802_" d="M72.16,99.73H9.927c-2.762,0-5,2.239-5,5v199.928c0,2.762,2.238,5,5,5H72.16c2.762,0,5-2.238,5-5V104.73 C77.16,101.969,74.922,99.73,72.16,99.73z"></path> <path id="XMLID_803_" d="M41.066,0.341C18.422,0.341,0,18.743,0,41.362C0,63.991,18.422,82.4,41.066,82.4 c22.626,0,41.033-18.41,41.033-41.038C82.1,18.743,63.692,0.341,41.066,0.341z"></path> <path id="XMLID_804_" d="M230.454,94.761c-24.995,0-43.472,10.745-54.679,22.954V104.73c0-2.761-2.238-5-5-5h-59.599 c-2.762,0-5,2.239-5,5v199.928c0,2.762,2.238,5,5,5h62.097c2.762,0,5-2.238,5-5v-98.918c0-33.333,9.054-46.319,32.29-46.319 c25.306,0,27.317,20.818,27.317,48.034v97.204c0,2.762,2.238,5,5,5H305c2.762,0,5-2.238,5-5V194.995 C310,145.43,300.549,94.761,230.454,94.761z"></path> </g> </g></svg>
+                        </a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            ))}
+            </div>
+        </div>
+     </section>
     </div>
   );
 };
