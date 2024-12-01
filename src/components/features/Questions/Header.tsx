6110db624cceb9851e780379ced5e68c6aa4266c
Lakshya Jain <minecraftpogtechno@gmail.com>
2024-12-01 13:03:10 -0500

    fix : no ESLint problems

--- src/components/features/Questions/Header.tsx
+++ src/components/features/Questions/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import { useRouter } from 'next/navigation'
 
 interface HeaderProps {
@@ -7,15 +6,7 @@ interface HeaderProps {
   question: string | undefined
 }
 
-const Header: React.FC<HeaderProps> = ({ name, question }) => {
-  const router = useRouter()
-
-  const linkedInShare = `https://www.linkedin.com/feed/?shareActive=true&text=${question} https://dailysat.org %23DailySAT`
-  const googleClassroomShare = `https://classroom.google.com/u/0/share?url=https://mywebsite.com&title=${question}&text=${question}resource%20on%20interserver%20communications!`
-  const shareRedirect = (link: string) => {
-    router.push(link)
-  }
-
+const Header: React.FC<HeaderProps> = ({ name }) => {
   return (
     <>
       <h1 className="text-3xl font-semibold">{name}</h1>
