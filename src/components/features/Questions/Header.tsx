736d6e2ae58e3088005f15cf12500d5306f17d2b
Hemit Patel <patelvhemit@gmail.com>
2024-12-02 20:34:55 -0500

    fixed clientside error

--- src/components/features/Questions/Header.tsx
+++ src/components/features/Questions/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 interface HeaderProps {
   name: string;
