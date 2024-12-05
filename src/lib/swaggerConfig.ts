1c531ef606be57a12b3e7d09800feb3f39952e2e
Hemit Patel <patelvhemit@gmail.com>
2024-12-05 13:33:35 -0500

    changed api specs

--- src/lib/swaggerConfig.ts
+++ src/lib/swaggerConfig.ts
@@ -2,11 +2,11 @@ import { createSwaggerSpec } from "next-swagger-doc";
 
 const getApiDocs = async () => {
   const spec = createSwaggerSpec({
-    apiFolder: "app/api", // define api folder under app folder
+    apiFolder: "src/app/api", // define api folder under app folder
     definition: {
       openapi: "3.0.0",
       info: {
-        title: "Next Swagger API Example",
+        title: "DailySAT API",
         version: "1.0",
       },
       security: [],
