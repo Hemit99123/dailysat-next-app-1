d5c57320135cd3f3a8effbd67553449d78e750e7
Hemit Patel <patelvhemit@gmail.com>
2024-12-05 11:02:22 -0500

    Update answer.ts

--- src/types/answer.ts
+++ src/types/answer.ts
@@ -1,7 +1,6 @@
 export type Answers = "A" | "B" | "C" | "D" 
 
 export function answerToString(answer : Answers){
-    // Typescript is dumb
     if(answer == "A")return "A";
     if(answer =="B")return "B";
     if(answer == "C")return "C";
