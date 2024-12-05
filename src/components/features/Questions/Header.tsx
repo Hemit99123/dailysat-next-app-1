1c06824a69b814b3d35b9029d6efbeae017cc86d
Hemit Patel <patelvhemit@gmail.com>
2024-12-05 11:05:05 -0500

    Update Header.tsx

--- src/components/features/Questions/Header.tsx
+++ src/components/features/Questions/Header.tsx
@@ -13,6 +13,7 @@ const Header: React.FC<HeaderProps> = ({ name, question }) => {
 
   const linkedInShare = `https://www.linkedin.com/feed/?shareActive=true&text=${question} https://dailysat.org %23DailySAT`
   const googleClassroomShare = `https://classroom.google.com/u/0/share?url=https://mywebsite.com&title=${question}&text=${question}resource%20on%20interserver%20communications!`
+ 
   const shareRedirect = (link: string) => {
     router.push(link)
   }
