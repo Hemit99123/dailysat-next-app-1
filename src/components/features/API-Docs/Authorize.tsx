891072d81af4b7eabd364018cd6822aa7db09fa8
Hemit Patel <patelvhemit@gmail.com>
2024-12-06 16:09:35 +0000

    added otp verification with frontend

--- src/components/features/API-Docs/Authorize.tsx
+++ src/components/features/API-Docs/Authorize.tsx
@@ -4,13 +4,14 @@ import { useState } from "react";
 export default async function IndexPage() {
 
     const [email, setEmail] = useState("")
+    const [otp, setOTP] = useState("")
 
     const handleAuthorization = async () => {
         const results = await axios.get(`/api/verification/find-employee?email=${email}`)
         
         if (results.data.result) {
             // this will generate the code and send it to the email given through mailgrid
-            await axios.post(`/api/verification/generate-code`, {
+            await axios.post(`/api/verification/otp`, {
                 email
             })            
         } else {
@@ -20,11 +21,31 @@ export default async function IndexPage() {
         }
     }
 
+    const handleVerification = async () => {
+      // since we have email in this scp[e. the email variable within the next loc will take the value of 
+      // the variable within this function
+
+      const email = prompt("What is your email?")
+
+      const results = await axios.get(`/api/verification/otp?passwordAttempt=${otp}&&email=${email}`)
+    
+      if (results.data.result) {
+        // assign a session because user has been authorized
+
+      } else {
+        alert("Wrong OTP")
+      }
+    }
+
   return (
     <section className="container">
-        <h1>Enter email:</h1>
+        <h1>Enter email: (to generate an OTP)</h1>
       <input onChange={(e) => setEmail(e.target.value)}/>
       <button onClick={handleAuthorization}>Submit</button>
+
+      <h1>Enter one time password:</h1>
+      <input onChange={(e) => setOTP(e.target.value)}/>
+      <button onChange={handleVerification}>Verify code</button>
     </section>
   );
 }
