1f240d2885b709d7596e0892d6fc5756c0b98412
Hemit Patel <hemitpatel@Hemits-MacBook-Air.local>
2024-12-11 21:44:02 -0500

    getting greeting based on time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 const Home = () => {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    const getGreeting = () => {
+      const hours = new Date().getHours();
+      if (hours < 12) {
+        return "Good morning!";
+      } else if (hours < 18) {
+        return "Good afternoon!";
+      } else {
+        return "Good evening!";
+      }
+    };
+    setGreeting(getGreeting());
+  }, []);
 
   return (
     <div>
-      dashboard
+      <h1>{greeting}</h1>
     </div>
   )
-};
+}
 
 export default Home;
