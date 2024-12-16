a2f628087a418a86b52e656f326cc3d590573a4b
Hemit Patel <hemitvpatel@gmail.com>
2024-12-15 21:50:57 -0500

    removed the coming soon page + added math skills to dataset

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -6,3 +6,9 @@ export const readingTopics: Topic[] = [
     { id: 3, name: "Expression of Ideas", description: "Topic 3" },
     { id: 4, name: "Standard English Conventions", description: "Topic 4" },
 ];
+
+export const mathTopics: Topic[] = [
+    { id: 1, name: "Algebra", description: "Topic 1" },
+    { id: 2, name: "Problem Solving", description: "Topic 2" },
+    { id: 3, name: "Data Analysis", description: "Topic 3" },
+];
