a7274190efb79dfa51ef59096a21357f9b3dd5dd
Hemit Patel <hemitvpatel@gmail.com>
2024-12-16 15:50:26 +0000

    added module to math to fix vercel deployment err

--- src/app/api/questions/math/route.ts
+++ src/app/api/questions/math/route.ts
@@ -1,1 +1,18 @@
 // backend work for math questions :)
+
+export async function GET(request: Request) {
+
+    try {
+        // Return the result with the question document
+        return Response.json({ 
+            message: "Coming soon!"
+         });
+      } catch (error) {
+        // Handle any database errors
+        return Response.json({
+          code: 500,
+          error: "Internal server error",
+          errorMsg: error
+        });
+      }
+} 
