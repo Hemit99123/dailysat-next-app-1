b075da9486331e49d07b2ace65e25585a67a044a
Hemit Patel <hemitvpatel@gmail.com>
2024-12-16 15:32:05 +0000

    remoduralzied the getstarted tab (used in both math n reading)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ import { useAnswerStore } from "@/store/answer";
 import { Topic } from "@/types/topic";
 import Sidebar from "@/components/features/Sidebar/Sidebar"; // Import Sidebar
 import {readingTopics} from '@/data/topics'
+import GetStarted from "@/components/features/Questions/GetStarted";
 
 export interface QuestionData {
   id: string;
@@ -197,15 +198,7 @@ const Home = () => {
             </div>
           </div>
         ) : (
-          <div className="flex flex-col items-center flex-grow">
-            <div className="mt-16 text-center">
-              <hr className="mx-8 my-5 h-px border-0 bg-gray-200" />
-              <p className="text-xl font-medium">Select a topic to start</p>
-              <p className="text-gray-400">
-                Get started by selecting a topic from the left.
-              </p>
-            </div>
-          </div>
+          <GetStarted />
         )}
       </div>
 
