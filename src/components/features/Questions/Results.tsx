48dd708b934a25c5f948788ca0d3c50f4508372d
Hemit Patel <hemitvpatel@gmail.com>
2024-12-19 19:53:57 -0500

    Update Results.tsx

--- src/components/features/Questions/Results.tsx
+++ src/components/features/Questions/Results.tsx
@@ -1,35 +1,46 @@
-import React, { MutableRefObject } from "react"
+import React, { MutableRefObject } from "react";
 
 interface ResultProps {
-    answerComponent: MutableRefObject<HTMLDivElement | null>
+    answerComponent: MutableRefObject<HTMLDivElement | null>;
     isAnswerCorrect: boolean | null;
     handleToggleEditorial: () => void;
-    openEditorial: boolean
+    openEditorial: boolean;
     explanation: string | undefined;
 }
 
-const Result: React.FC<ResultProps> = ({answerComponent, isAnswerCorrect, handleToggleEditorial, openEditorial, explanation}) => {
+const Result: React.FC<ResultProps> = ({
+    answerComponent,
+    isAnswerCorrect,
+    handleToggleEditorial,
+    openEditorial,
+    explanation
+}) => {
     return (
         <div className="mt-4 pl-7 pb-10" ref={answerComponent}>
-            {isAnswerCorrect ? (
-                <p className="text-green-500 text-lg font-semibold">
-                    You are correct!
-                </p>
+            {isAnswerCorrect !== null && (
+                isAnswerCorrect ? (
+                    <p className="text-green-500 text-lg font-semibold">
+                        You are correct!
+                    </p>
                 ) : (
                     <div>
-                    <p className="text-red-500 text-lg font-semibold">
-                    You are wrong :(
-                    </p>
-                    <button onClick={handleToggleEditorial}>
-                        Do you want to see the editorials? Click here!
-                    </button>
-                    {openEditorial && (
-                        <p className="mt-6">{explanation}</p>
-                    )}
+                        <p className="text-red-500 text-lg font-semibold">
+                            You are wrong :(
+                        </p>
+                        <button
+                            onClick={handleToggleEditorial}
+                            className="text-blue-500 underline mt-2"
+                        >
+                            Do you want to see the editorials? Click here!
+                        </button>
+                        {openEditorial && explanation && (
+                            <p className="mt-6">{explanation}</p>
+                        )}
                     </div>
-                  )}
+                )
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default Result
+export default Result;
