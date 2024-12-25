f44d94b2f17f37c931f13f00edbd8a724239871d
Hemit Patel <hemitvpatel@gmail.com>
2024-12-25 00:17:51 -0500

    Update page.tsx

--- src/app/math/page.tsx
+++ src/app/math/page.tsx
@@ -1,35 +1,39 @@
-"use client"
+"use client";
 
-import Sidebar from "@/components/features/Sidebar/Sidebar";
-import { mathTopics } from '@/data/topics'; 
-import MathSVG from "@/components/features/Questions/icons/MathSVG";
-import { Topic } from "@/types/topic";
 import { useEffect, useRef } from "react";
+import Sidebar from "@/components/features/Sidebar/Sidebar";
+import { mathTopics } from "@/data/topics";
+import MathSVG from "@/components/features/Questions/icons/MathSVG";
 import MathQuestion from "@/components/features/Questions/Question-UI/MathQuestion";
 import Header from "@/components/features/Questions/Header";
-import { useAnswerCounterStore } from "@/store/score";
 import ScoreModal from "@/components/features/Questions/Modals/ScoreModal";
 import StreakModal from "@/components/features/Questions/Modals/StreakModal";
-import { Answers } from "@/types/answer";
-import { useScoreModalStore, useStreakAnnouncerModalStore, useStreakCounterModalStore } from "@/store/modals";
 import StreakAnnouncer from "@/components/features/Questions/Modals/StreakAnnouncer";
-import useQuestionHandler from "@/hooks/questions";
-import Spinner from "@/components/common/Spinner";
 import GetStarted from "@/components/features/Questions/GetStarted";
 import Result from "@/components/features/Questions/Results";
-import { useQuestionStore, useTopicStore } from "@/store/questions";
+import Spinner from "@/components/common/Spinner";
 import QuestionWrappers from "@/components/wrappers/question/Question";
 import MainWrappers from "@/components/wrappers/question/Main";
+import useQuestionHandler from "@/hooks/questions";
+import { useAnswerCounterStore } from "@/store/score";
+import {
+  useScoreModalStore,
+  useStreakAnnouncerModalStore,
+  useStreakCounterModalStore,
+} from "@/store/modals";
+import { useQuestionStore, useTopicStore } from "@/store/questions";
+import { Answers, Topic } from "@/types";
 
 const Math = () => {
-  const {fetchRandomQuestion, handleAnswerSubmit, handleCheckThreeStreak} = useQuestionHandler()
-  const selectedTopic = useTopicStore((state) => state.selectedTopic)
-  const setSelectedTopic = useTopicStore((state) => state.setSelectedTopic)
-  const randomQuestion = useQuestionStore((state) => state.randomQuestion)
-  const setRandomQuestion = useQuestionStore((state) => state.setRandomQuestion)
+  const { fetchRandomQuestion, handleAnswerSubmit, handleCheckThreeStreak } = useQuestionHandler();
 
-  const correctCount = useAnswerCounterStore((state) => state.count)
-  const answerCorrectRef: Record<Answers, number> = { A: 0, B: 1, C: 2, D: 3 };
+  const selectedTopic = useTopicStore((state) => state.selectedTopic);
+  const setSelectedTopic = useTopicStore((state) => state.setSelectedTopic);
+
+  const randomQuestion = useQuestionStore((state) => state.randomQuestion);
+  const setRandomQuestion = useQuestionStore((state) => state.setRandomQuestion);
+
+  const correctCount = useAnswerCounterStore((state) => state.count);
 
   const isScoreModalOpen = useScoreModalStore((state) => state.isOpen);
   const isAnnouncerModalOpen = useStreakAnnouncerModalStore((state) => state.isOpen);
@@ -37,13 +41,15 @@ const Math = () => {
 
   const answerComponent = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    setRandomQuestion(null)
-    setSelectedTopic(null)
-  }, [])
+  const answerCorrectRef: Record<Answers, number> = { A: 0, B: 1, C: 2, D: 3 };
 
   useEffect(() => {
-    handleCheckThreeStreak()
+    setRandomQuestion(null);
+    setSelectedTopic(null);
+  }, [setRandomQuestion, setSelectedTopic]);
+
+  useEffect(() => {
+    handleCheckThreeStreak();
   }, [correctCount, handleCheckThreeStreak]);
 
   const handleTopicClick = (topic: Topic) => {
@@ -51,16 +57,13 @@ const Math = () => {
     fetchRandomQuestion("Math", topic);
   };
 
-
   if (isScoreModalOpen || isStreakModalOpen) {
     return (
       <>
-        <ScoreModal />
-        <StreakModal />
+        {isScoreModalOpen && <ScoreModal />}
+        {isStreakModalOpen && <StreakModal />}
       </>
     );
-  };
-
   }
 
   return (
@@ -72,35 +75,28 @@ const Math = () => {
         handleTopicClick={handleTopicClick}
       />
 
-      {/* Main Content */}
       <QuestionWrappers>
         <span className="font-bold text-lg border-2 border-black rounded-lg lg:w-1/5 text-center cursor-pointer hover:bg-black hover:text-white duration-500">
           ✨ Experimental ✨
         </span>
         {selectedTopic ? (
           <div className="w-full mx-auto">
-
-            <Header
-                name={selectedTopic.name}
-                question={randomQuestion?.question}
-            />
+            <Header name={selectedTopic.name} question={randomQuestion?.question} />
             {randomQuestion ? (
-              <MathQuestion
-                onAnswerSubmit={() => 
-                  handleAnswerSubmit( 
-                    "Math",
-                    randomQuestion.correctAnswer, 
-                    answerCorrectRef
-                  )
-                }
-              />
+              <>
+                <MathQuestion
+                  onAnswerSubmit={() =>
+                    handleAnswerSubmit("Math", randomQuestion.correctAnswer, answerCorrectRef)
+                  }
+                />
+                <Result
+                  answerComponent={answerComponent}
+                  explanation={randomQuestion?.explanation || ""}
+                />
+              </>
             ) : (
               <Spinner />
             )}
-              <Result 
-                answerComponent={answerComponent}
-                explanation={randomQuestion?.explanation || ""}
-              />
           </div>
         ) : (
           <GetStarted />
