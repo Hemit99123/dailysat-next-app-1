876a7647be075108d291d222749281e4a458a5e0
Hemit Patel <hemitvpatel@gmail.com>
2024-12-25 10:51:20 -0500

    Create menuItem.ts

---
+++ src/data/menuItem.ts
@@ -0,0 +1,6 @@
+// Define the menu items for navigation
+export const menuItems = [
+  { label: "Reading and Writing", href: "/reading"},
+  { label: "Math", href: "/math"},
+  { label: "About", href: "/about" },
+];
