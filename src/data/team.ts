6ed5e81e1a5bfb4eb93753044bb9bc2622ba4af2
Hemit Patel <hemitvpatel@gmail.com>
2024-12-24 23:53:33 -0500

    changed location for team data

---
+++ src/data/team.ts
@@ -0,0 +1,33 @@
+export const team = [
+    {
+      name: 'Aarush Kute',
+      role: 'Chief Executive Officer',
+      imgSrc: './people/aarush.png',
+      linkedIn: "https://www.linkedin.com/in/aarush-kute-1639a525b/"
+    },
+    {
+      name: 'Hemit Patel',
+      role: 'President & Chief Operating Officer',
+      imgSrc: 'https://media.licdn.com/dms/image/v2/D5603AQFDJxp7vmgX0Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1695640460890?e=1738195200&v=beta&t=sfsEBemwKB0_r70CJcFTqoBPOa2LgWU46Tq-CtjgJek',
+      linkedIn: "https://www.linkedin.com/in/hemit-patel-383ab3271/"
+    }, 
+    {
+      name: "William Chan",
+      role: "Head of Marketing",
+      imgSrc: "https://media.licdn.com/dms/image/v2/D5603AQFMtNWPnihUBg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1718285605821?e=1738800000&v=beta&t=A2mf419HvUG8s4vdNi_10pq1VRPljzekz7BtfqWrImU",
+      linkedIn: "https://www.linkedin.com/in/wilman-chan-03a468286/"
+    },
+      {
+      name: "Lakshaya Jain",
+      role: "Chief Technical Officer",
+      imgSrc: "./people/Lakshaya.png",
+      linkedIn: "https://www.linkedin.com/in/lakshya-jain-9a66a22a5/"
+    },
+    {
+      name: "Devesh Khilnani",
+      role: "Chief Business Officer",
+      imgSrc: "https://media.licdn.com/dms/image/v2/D4D03AQHp9U6V0ER0bw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1724947190257?e=1738800000&v=beta&t=HcqzWskjTN-TnRsu9sfjUdFe-aTl2S2ZrSUojpB4UJw",
+      linkedIn: "https://www.linkedin.com/in/dkhilnani/"
+    }
+    
+];
