06903e8942d5bf6ea0f485ea6db402c4192a8e83
Hemit Patel <hemitvpatel@gmail.com>
2025-01-01 13:32:33 -0500

    changed header

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -75,7 +75,7 @@ const About = () => {
       <section className="bg-white">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
             <div className="mx-auto mb-8 max-w-screen-sm lg:mb-16">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-blue-900">Our Team</h2>
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-blue-900">Our Executives</h2>
             <p className="font-light text-gray-500 sm:text-xl">
                 These are the people behind DailySAT!
             </p>
