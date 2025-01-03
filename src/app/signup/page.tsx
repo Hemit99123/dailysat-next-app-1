0a2e4ebfd548f208e4bac0c241f08a46416060de
Hemit Patel <hemitvpatel@gmail.com>
2025-01-03 10:45:07 -0500

    added authjs frontend code
    - first time using server actions!!

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,115 +1,20 @@
 "use client"
 
-import {
-  CredentialResponse,
-  GoogleLogin,
-  GoogleOAuthProvider
-} from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
-import React, {useEffect} from "react";
-import { getCookieConsentValue, resetCookieConsentValue } from "react-cookie-consent";
-import { DBQuestionRecord } from "@/types/questions";
-import axios from "axios";
-import useUserStore, { useLoggedInStore } from "@/store/user";
+import { handleSignIn } from './action'
+import React from 'react'
+import GoogleButton from 'react-google-button'
 
-export interface User {
-  // MongoDB string 
-  _id?: string,
-  id?: string,
-  email?: string,
-  name?: string,
-  picture?: string,
+const page = () => {
 
-  // Given name is the first name
-  given_name?: string,
-
-  // Currency
-  currency?: number,
-
-  // Questions answered
-  questionsAnswered?: DBQuestionRecord[]
-}
-
-// Endstate of API request
-export type AuthCode = "login" | "signup";
-
-// The type of the return data
-export interface AuthResponse {
-  user?: User,
-  code?: number,
-  message?: string,
-  status?: AuthCode,
-  ts?: string
-}
-
-export default function Signup() {
-  useEffect(() => {
-  }, [])
-  
-  async function successCallback(token: CredentialResponse) {
-    const str: string = token.credential || "";
-    const user: User = jwtDecode<User>(str);
-    user.currency = 0;
-    user.questionsAnswered = [];
-
-    if (getCookieConsentValue("userData") == "true") {
-      // get userData
-      const userData: AuthResponse = await (await axios.post("/api/auth", user)).data;
-
-      if (userData.code != 200)
-        window.alert(userData.message)
-      else {
-        // Save
-        localStorage.setItem("USER", JSON.stringify(userData.user));
-        localStorage.setItem("loggedin","true");
-        useUserStore.setState({ user: userData.user });
-        useLoggedInStore.setState({ loggedIn: true });
-
-        // TODO : Redirect to dashboard
-        window.location.replace("/");
-      }
-    }
-
-    else {
-      // Alert
-      window.alert("Enable the cookies, or else we won't be able to create an account!");
-    }
-  }
-
-  function errorCallback() {
-    // TO DO : Toast system
-    alert("You have to enable the cookies to signup!");
-    resetCookieConsentValue();
-  }
 
   return (
-    <div>
-      <GoogleOAuthProvider
-        clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}
-      >
-        <div className="bg-white p-8">
-          <div className="flex mb-6">
-            <button
-              className={`flex-1 py-2 text-blue-600 text-4xl font-bold`}
-            >
-              Sign In 
-
-              <div className="flex justify-center mt-10">
-                <GoogleLogin
-                  onSuccess={successCallback}
-                  onError={() => errorCallback()}
-                  //   useOneTap
-                  shape="pill"
-                  width={500}
-                />
-              </div>
-            </button>
-          </div>
-        </div>
-        {/* TO DO : Actual UI */}
-
-        ;
-      </GoogleOAuthProvider>
+    <div className='flex flex-col items-center mt-10'>
+      <h1 className='text-blue-800 font-bold text-4xl mb-10'>Sign in</h1>
+      <GoogleButton 
+        onClick={() => handleSignIn()}
+      />
     </div>
-  );
+  )
 }
+
+export default page
