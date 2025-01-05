c24496a0df637dc7a545539e8cb4dc231f573a37
Hemit Patel <hemitvpatel@gmail.com>
2025-01-04 21:00:50 -0500

    fixed??

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -12,7 +13,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
       },
     }),
+    GitHub,
   ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
   trustHost: true,
   secret: process.env.AUTH_SECRET
 });
