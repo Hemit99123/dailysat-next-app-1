148821eecb3587a2b216bfa6c589c55b8f408655
Hemit Patel <hemitvpatel@gmail.com>
2025-01-27 16:31:42 -0500

    removed all logic for employee session (now moved to dailysat-admin)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,8 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import handleEmployeeRoutes from '@/lib/middleware/auth/employee';
 import handleUserRoutes from '@/lib/middleware/auth/reg-user';
 
 export const middleware = async (request: NextRequest) => {
-  const employeeResponse = await handleEmployeeRoutes(request);
-  if (employeeResponse) return employeeResponse;
-
   const userResponse = await handleUserRoutes(request);
   if (userResponse) return userResponse;
 
