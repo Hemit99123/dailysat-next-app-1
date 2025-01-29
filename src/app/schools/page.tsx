89ad4bd09e34da63f84f41d9cf7caced3a1f924c
Hemit Patel <hemitvpatel@gmail.com>
2025-01-29 14:17:04 -0500

    made grid layout responsive

--- src/app/schools/page.tsx
+++ src/app/schools/page.tsx
@@ -21,7 +21,7 @@ const Schools = () => {
 
             <div>
                 <h3 className="text-2xl font-semibold mb-9">Partnered Schools:</h3>
-                <div className="grid grid-cols-3 mx-auto gap-16">
+                <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto lg:gap-16">
                     <SchoolItem 
                         name="Demo"
                         location="Virtual"
