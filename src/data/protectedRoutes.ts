c78cbac8caa8ca75b6bb79130a6dab7a47d1abdb
Hemit Patel <hemitvpatel@gmail.com>
2025-01-29 17:54:26 -0500

    updated protected routes

--- src/data/protectedRoutes.ts
+++ src/data/protectedRoutes.ts
@@ -1,8 +1,5 @@
 const protectedRoutes = {
-    protectedAuth: ['/', '/shop', '/math', '/reading-writing'],
-    protectedEmployeeBackend: ['/api/protected-employee'],
-    protectedEmployeeFrontend: ['/api-docs'],
-    employeeAuthorize: ['/employee-authorize', '/employee-authorize/logout'],
+    protectedAuth: ['/', '/shop', '/math', '/reading-writing', '/schools'],
 };
 
 export default protectedRoutes
