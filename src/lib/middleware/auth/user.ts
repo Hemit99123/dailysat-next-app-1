3ebf94c872da97a9882631207dec83284c7ed6d9
Hemit Patel <hemitvpatel@gmail.com>
2025-02-04 21:11:30 -0500

    removed unused imports (lint error)

--- src/lib/middleware/auth/user.ts
+++ src/lib/middleware/auth/user.ts
@@ -2,7 +2,6 @@ import { auth } from "@/auth";
 import protectedRoutes from "@/data/protectedRoutes";
 import type { NextRequest } from "next/server";
 import redirectTo from "../common/redirect";
-import { handleSignIn } from "@/components/common/server-actions";
 
 const handleUserRoutes = async (request: NextRequest) => {
     const session = await auth();
